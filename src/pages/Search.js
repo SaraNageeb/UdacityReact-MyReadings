7810aa4acecc1776ab0db0295a63785c747ff9b4
SaraNageeb <sara.nageeb27@gmail.com>
2022-11-03 14:25:18 +0200

    search works

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,25 +1,68 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import * as BooksAPI from "../BooksAPI";
+import Book from "../components/Book";
 
-const Search = () => {
+const Search = ({ Shelfbooks, ChangeBookShelf }) => {
+  const [searchBooks, setSearchBooks] = useState([]);
+  const [query, setQuery] = useState("");
+
+  const handleSearch = (e) => {
+    setQuery(e.target.value);
+    searchBook(e.target.value);
+  };
+  const searchBook = async (query) => {
+    if (!query) {
+      setSearchBooks([]);
+    }
+    if (query.trim()) {
+      const res = await BooksAPI.search(query.trim(), 20);
+
+      if (res && !res.error) {
+        const joinedBooks = res.map((book) => {
+          Shelfbooks.forEach((Shelfbook) => {
+            if (book.id === Shelfbook.id) {
+              book = Shelfbook;
+            }
+          });
+          return book;
+        });
+        setSearchBooks(joinedBooks);
+      }
+    }
+  };
   return (
     <div className="search-books">
-          <div className="search-books-bar">
-          <Link className="close-search" to="/">
-                    Close
-            </Link>
-            <div className="search-books-input-wrapper">
-              <input
-                type="text"
-                placeholder="Search by title, author, or ISBN"
-              />
-            </div>
-          </div>
-          <div className="search-books-results">
-            <ol className="books-grid"></ol>
-          </div>
+      <div className="search-books-bar">
+        <Link className="close-search" to="/">
+          Close
+        </Link>
+        <div className="search-books-input-wrapper">
+          <input
+            type="text"
+            placeholder="Search by title, author, or ISBN"
+            value={query}
+            onChange={handleSearch}
+          />
         </div>
-          
-  )
-}
+      </div>
+      <div className="search-books-results">
+        <ol className="books-grid">
+          {query.length > 0 && searchBooks.length > 0 ? (
+            searchBooks.map((book) => {
+              return (
+                <li key={book.id}>
+                  <Book book={book} ChangeBookShelf={ChangeBookShelf} />
+                </li>
+              );
+            })
+          ) : (
+            <p>No books to show - search in the bar above.</p>
+          )}
+        </ol>
+      </div>
+    </div>
+  );
+};
 
-export default Search
+export default Search;
